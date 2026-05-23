b929f8462999c91cbeb362db8fa9578b8914cd99
klarka26 <cafalovaklara@gmail.com>
2026-05-24 00:43:27 +0200

    uprava validacie poloziek

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -3,5 +3,5 @@ class ExpenseItem < ApplicationRecord
 
   validates :description, presence: true
   validates :quantity, numericality: { greater_than: 0 }
-  validates :unit_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :unit_price, numericality: true
 end
