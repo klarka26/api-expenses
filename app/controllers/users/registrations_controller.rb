2bc1fe028de2884ce4ab7e136ea7d6e7abfafb0d
klarka26 <cafalovaklara@gmail.com>
2026-05-24 00:17:13 +0200

    registracia upravenie 4

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,16 +11,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     )
   end
 
-  def sign_up(resource_name, resource)
-  end
-
   def respond_with(resource, _opts = {})
     if resource.persisted?
-      token = request.env["warden-jwt_auth.token"]
 
       render json: {
         message: "Používateľ úspešne vytvorený.",
-        token: token,
         user: resource.as_json(only: [ :id, :email, :first_name, :last_name, :role ])
       }, status: :created
     else
