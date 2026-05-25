61e63619218e2201a3e636759bd1b6cd9c533719
klarka26 <cafalovaklara@gmail.com>
2026-05-25 17:05:06 +0200

    predanie tabulky hidden expense

--- db/migrate/20260525143712_hidden_expenses.rb
+++ db/migrate/20260525143712_hidden_expenses.rb
@@ -1,4 +1,4 @@
-class CreateHiddenExpenses < ActiveRecord::Migration[8.0]
+class HiddenExpenses < ActiveRecord::Migration[8.0]
   def change
     create_table :hidden_expenses do |t|
       t.references :user, null: false, foreign_key: true
